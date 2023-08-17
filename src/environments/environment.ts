ea017404f030bd6e5db34dd42091d83c670d9f10
fixlon <fixlon2001@gmail.com>
2023-08-17 19:35:49 +0530

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
-// src/environments/environment.ts
-
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000', // BASE API URL
   url: 'http://localhost:3000/usersprofile',
+
 };
